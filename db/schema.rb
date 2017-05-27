342a4df234463d39c9a3f1c0ed09f7c90bd0f6d7
dongwonkay <dongwon@likelion.org>
2017-05-27 07:46:59 +0000

    페북 , 로그인 합침

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170527071321) do
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-=======
 ActiveRecord::Schema.define(version: 20170527070053) do
 
   create_table "users", force: :cascade do |t|
@@ -40,5 +31,4 @@ ActiveRecord::Schema.define(version: 20170527070053) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
->>>>>>> fb9fe14f01cb0daf99eed250ad80fdf2b4742a4b
 end
